df59ee124b44abf9dade5d9737306f9279233965
cisco <cisco@UPOAULA10310>
2024-06-26 11:34:21 -0500

    Agregar archivos de proyecto.

---
+++ LeonardoAndrade_LaboratorioApodNasa/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LeonardoAndrade_LaboratorioApodNasa
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LeonardoAndrade_LaboratorioApodNasa/Services/ApodServiceLA.cs
@@ -0,0 +1,38 @@
+﻿using LeonardoAndrade_LaboratorioApodNasa.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeonardoAndrade_LaboratorioApodNasa.Services
+{
+    public class ApodServiceLA
+    {
+        public async Task<LAApod> GetImage(DateTime dt)
+        {
+            LAApod dto = null;
+            HttpResponseMessage response;
+            string requestUrl = $"https://api.nasa.gov/planetary/apod?date={dt.ToString("yyyy-MMdd")}&api_key=tJqCXGEUQy1Gk3K4XXbEPDdrtl3hQWvIjCORU9HN";
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+                HttpClient client = new HttpClient();
+                response = await client.SendAsync(request);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    dto = JsonConvert.DeserializeObject<LAApod>(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                throw;
+            }
+            return dto;
+        }
+
+    }
+}
